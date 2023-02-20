b66bdef194f36a4dee6b95d9f66b3fa266ca14d7
atillakokcu <kokcu.ati@gmail.com>
2023-02-21 00:35:53 +0300

    test

---
+++ MvcOnlineCommerceAutomation/Models/Classes/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineCommerceAutomation.Models.Classes
+{
+    public class Test
+    {
+    }
+}
