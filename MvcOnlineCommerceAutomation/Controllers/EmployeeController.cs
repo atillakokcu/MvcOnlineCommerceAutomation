f2ac64d787531decb7c67c808f7bdc3df3e4d21a
atillakokcu <kokcu.ati@gmail.com>
2023-02-21 23:03:09 +0300

    Personel ve Ürün controllerları girildi

---
+++ MvcOnlineCommerceAutomation/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineCommerceAutomation.Models.Classes;
+
+namespace MvcOnlineCommerceAutomation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        Context contex = new Context();
+        public ActionResult Index()
+        {
+            var degerler = contex.Employees.ToList();
+
+            return View(degerler);
+        }
+
+
+        [HttpGet]
+        public ActionResult PersonelEkle()
+        {
+
+            List<SelectListItem> deger1 = (from x in contex.Departments.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmentName,
+                                               Value = x.DepartmentId.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PersonelEkle(Employee employee)
+        {
+            contex.Employees.Add(employee);
+            contex.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult PersonelGetir(int Id)
+        {
+            List<SelectListItem> deger1 = (from x in contex.Departments.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.DepartmentName,
+                                               Value = x.DepartmentId.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+            Employee personel = contex.Employees.Find(Id);
+
+            return View("PersonelGetir",personel);
+
+        }
+
+        public ActionResult PersonelGuncelle(Employee Personel)
+        {
+            var personel = contex.Employees.Find(Personel.EmployeeID);   
+            personel.EmployeeName = Personel.EmployeeName;
+            personel.EmployeeImage = Personel.EmployeeImage;
+            personel.EmployeeSurname= Personel.EmployeeSurname;
+            personel.DepartmentId= Personel.DepartmentId;
+            contex.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+
+    }
+}
---
+++ MvcOnlineCommerceAutomation/Controllers/SalesController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using MvcOnlineCommerceAutomation.Models.Classes;
+
+namespace MvcOnlineCommerceAutomation.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        Context contex = new Context();
+        public ActionResult Index()
+        {
+            var degerler = contex.SalesActions.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            List<SelectListItem> deger1 = (from x in contex.Products.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.ProductName,
+                                               Value = x.ProductId.ToString(),
+                                           }).ToList();
+
+            List<SelectListItem> deger2 = (from x in contex.Customers.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CustomerName + " " + x.CustomerSurname,
+                                               Value = x.CustomerId.ToString(),
+                                           }).ToList();
+
+
+            List<SelectListItem> deger3 = (from x in contex.Employees.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.EmployeeName + " " + x.EmployeeSurname,
+                                               Value = x.EmployeeID.ToString(),
+                                           }).ToList();
+
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            ViewBag.dgr3 = deger3;
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SalesAction sales)
+        {
+            contex.SalesActions.Add(sales);
+            contex.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
