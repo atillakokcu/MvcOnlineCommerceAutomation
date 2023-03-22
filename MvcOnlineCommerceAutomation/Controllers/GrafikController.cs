7cfc54bac6384d59a4c64128e29cb5681280d0e0
atillakokcu <kokcu.ati@gmail.com>
2023-03-22 22:54:57 +0300

    grafik ekranının oluşturulması

---
+++ MvcOnlineCommerceAutomation/Controllers/GrafikController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcOnlineCommerceAutomation.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
