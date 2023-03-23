b3fd3a95a0053b95b4690db2b87a469529c083f9
atillakokcu <kokcu.ati@gmail.com>
2023-03-23 23:23:45 +0300

    grafikleri entegre etme

--- MvcOnlineCommerceAutomation/Controllers/GrafikController.cs
+++ MvcOnlineCommerceAutomation/Controllers/GrafikController.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MvcOnlineCommerceAutomation.Models.Classes;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace MvcOnlineCommerceAutomation.Controllers
@@ -9,9 +12,79 @@ namespace MvcOnlineCommerceAutomation.Controllers
     public class GrafikController : Controller
     {
         // GET: Grafik
+        Context contex = new Context();
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult Index2()
+        {
+            var grafikciz = new Chart(600, 600);
+            grafikciz.AddTitle("Kategori- Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler",
+                xValue: new[] { "Mobilya", "Ofis Eşyaları", "Bilgisayar" }, yValues: new[] { 85, 66, 88 }).Write();
+
+            return File(grafikciz.ToWebImage().GetBytes(), "image/jpeg");
+
+        }
+
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var sonuclar = contex.Products.ToList();
+            sonuclar.ToList().ForEach(x => xvalue.Add(x.ProductName));
+            sonuclar.ToList().ForEach(y => yvalue.Add(y.ProductStock));
+            var grafik = new Chart(800, 800).AddTitle("Stoklar")
+                .AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafik.ToWebImage().GetBytes(), "image/jpeg");
+
+        }
+
+        public ActionResult Index4()
+        {
+
+            return View();
+        }
+
+        public ActionResult VisualizeUrunResult()
+        {
+
+            return Json(Urunlistesi(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Sinif1> Urunlistesi()
+        {
+
+            List<Sinif1> snf = new List<Sinif1>();
+            snf.Add(new Sinif1()
+            {
+                UrunAd="bilgisayar",
+                Stok=120
+            });
+            snf.Add(new Sinif1()
+            {
+                UrunAd="beyaz eşya",
+                Stok=150
+            });
+            snf.Add(new Sinif1()
+            {
+                UrunAd = "mobilya",
+                Stok = 70
+            });
+            snf.Add(new Sinif1()
+            {
+                UrunAd = "küçük ev aletleri",
+                Stok = 180
+            });
+            snf.Add(new Sinif1()
+            {
+                UrunAd = "mobil cihazlar",
+                Stok = 90
+            });
+
+            return snf;
+        }
+
     }
 }
---
+++ MvcOnlineCommerceAutomation/Models/Classes/Sinif1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineCommerceAutomation.Models.Classes
+{
+    public class Sinif1
+    {
+        public string UrunAd { get; set; }
+        public int Stok { get; set; }
+    }
+}
